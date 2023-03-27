fd3b94c58709190429243a988b123a5de875f7a3
Nguyễn Huy Hoàng <nghuyhoang2509@gmail.com>
2023-03-28 00:01:09 +0700

    test add element

--- pages/Chat/index.c
+++ pages/Chat/index.c
@@ -10,15 +10,48 @@
 GtkBuilder *builder;
 GtkWidget *window;
 GtkCssProvider *provider;
+GtkListBox *listbox;
 
 char data_message[14][100] = {"Hi hello hello hello hello hello hello hello hello hello hello", "Hello", "How are you today", "I'm good, And you", "I'm slepy", "What do you doing", "I play game", "yub, what is your game", "LOL, do you want enjoy with me", "Ok", "What time do you want play", "hmmmm", "Tonight", "20h"};
 int data_message_type[14] = {0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0};
 int n = 14;
 
+static void add_item_to_list_box(char *message, int message_type);
+
+static void btn_send_message_clicked()
+{
+    add_item_to_list_box("test thoi nha", 1);
+}
+
 void css_set(GtkWidget *g_widget, GtkCssProvider *provider)
 {
     GtkStyleContext *context = gtk_widget_get_style_context(g_widget);
     gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+}
+
+static void add_item_to_list_box(char *message, int message_type)
+{
+    GtkWidget *label = gtk_label_new(message);
+    GtkWidget *row = gtk_list_box_row_new();
+    GtkStyleContext *context = gtk_widget_get_style_context(row);
+    gtk_style_context_add_class(context, "container_message");
+
+    if (message_type == 0)
+    {
+        gtk_widget_set_halign(row, GTK_ALIGN_START);
+        gtk_style_context_add_class(context, "partner_message");
+    }
+    else
+    {
+        printf("click");
+
+        gtk_widget_set_halign(row, GTK_ALIGN_END);
+        gtk_style_context_add_class(context, "my_message");
+    }
+    gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
+    gtk_label_set_line_wrap_mode(GTK_LABEL(label), PANGO_WRAP_WORD);
+    gtk_container_add(GTK_CONTAINER(row), label);
+    gtk_list_box_insert(listbox, row, -1);
 }
 
 int main(int agrc, char *agrv[])
@@ -37,29 +70,12 @@ int main(int agrc, char *agrv[])
     GtkWidget *box = GTK_WIDGET(gtk_builder_get_object(builder, "box"));
     GtkWidget *container_list_message = GTK_WIDGET(gtk_builder_get_object(builder, "container_list_message"));
     GtkWidget *input_message = GTK_WIDGET(gtk_builder_get_object(builder, "input_message"));
+    GtkWidget *btn_send_message = GTK_WIDGET(gtk_builder_get_object(builder, "btn_send_message"));
 
-    GtkListBox *listbox = GTK_LIST_BOX(container_list_message);
+    listbox = GTK_LIST_BOX(container_list_message);
     for (int i = 0; i <= n; i++)
     {
-        GtkWidget *label = gtk_label_new(data_message[i]);
-        GtkWidget *row = gtk_list_box_row_new();
-        GtkStyleContext *context = gtk_widget_get_style_context(row);
-        gtk_style_context_add_class(context, "container_message");
-
-        if (data_message_type[i] == 0)
-        {
-            gtk_widget_set_halign(row, GTK_ALIGN_START);
-            gtk_style_context_add_class(context, "partner_message");
-        }
-        else
-        {
-            gtk_widget_set_halign(row, GTK_ALIGN_END);
-            gtk_style_context_add_class(context, "my_message");
-        }
-        gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
-        gtk_label_set_line_wrap_mode(GTK_LABEL(label), PANGO_WRAP_WORD);
-        gtk_container_add(GTK_CONTAINER(row), label);
-        gtk_list_box_insert(listbox, row, i);
+        add_item_to_list_box(data_message[i], data_message_type[i]);
     }
     gtk_container_foreach(GTK_CONTAINER(listbox), (GtkCallback)css_set, provider);
 
@@ -70,6 +86,7 @@ int main(int agrc, char *agrv[])
     css_set(window, provider);
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(btn_send_message, "clicked", G_CALLBACK(btn_send_message_clicked), NULL);
     gtk_widget_show_all(window);
 
     gtk_main();
