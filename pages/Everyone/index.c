796547970974cbb0679913adc06996e9e43f61f6
Nguyễn Huy Hoàng <nghuyhoang2509@gmail.com>
2023-03-28 01:37:30 +0700

    finish glade everyone

--- pages/Everyone/index.c
+++ pages/Everyone/index.c
@@ -1,46 +1,43 @@
 
-static GtkWidget *window;
-static GtkListStore *list_store;
-GtkTreeIter iter;
+static char data_mail[4][100] = {"nghuyhoang2509@gmail.com", "nhhhoctap@gmail.com", "crasmaverkgkg@gmail.com", "hoangnhse182096@fpt.edu.vn"};
 
-static void add_row_to_list_store(char *id, char *mail)
+int Everyone(int agrc, char *agrv[])
 {
-    gtk_list_store_append(list_store, &iter);
-    gtk_list_store_set(list_store, &iter, 0, id, -1);
-    gtk_list_store_set(list_store, &iter, 1, mail, -1);
-}
+    char data[2];
+    sprintf(data, "{}");
+    char *response = request("everyone", data);
+    printf("%s", response);
 
-int Everyone()
-{
     GtkBuilder *builder = gtk_builder_new_from_file("./pages/Everyone/Everyone.glade");
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "everyone_page"));
-    GtkWidget *label_name = GTK_WIDGET(gtk_builder_get_object(builder, "label_name"));
-    GtkTreeView *list_everyone = GTK_TREE_VIEW(gtk_builder_get_object(builder, "list_everyone"));
-    list_store = GTK_LIST_STORE(gtk_builder_get_object(builder, "list_store"));
-    GtkTreeViewColumn *id_col = GTK_TREE_VIEW_COLUMN(gtk_builder_get_object(builder, "id_col"));
-    GtkTreeViewColumn *mail_col = GTK_TREE_VIEW_COLUMN(gtk_builder_get_object(builder, "mail_col"));
-    GtkCellRenderer *id_row = GTK_CELL_RENDERER(gtk_builder_get_object(builder, "id_row"));
-    GtkCellRenderer *mail_row = GTK_CELL_RENDERER(gtk_builder_get_object(builder, "mail_row"));
-    GtkTreeSelection *select_list = GTK_TREE_SELECTION(gtk_builder_get_object(builder, "select_list"));
+    GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "everyone_page"));
+
     GtkCssProvider *provider = gtk_css_provider_new();
-
-    gtk_tree_view_column_add_attribute(id_col, id_row, "text", 0);
-    gtk_tree_view_column_add_attribute(mail_col, mail_row, "text", 1);
-
-    add_row_to_list_store("asdjasdhadshsad", "nghuyhoang2509@gmail.com");
-    add_row_to_list_store("asdjasdhadshsadsdasdd", "nghuyhoang2509@gmail.com");
-    add_row_to_list_store("asdjasdhadsh", "nghuyhoang2509@gmail.com");
-    add_row_to_list_store("asdjasdhadshsaddd", "nghuyhoang2509@gmail.com");
-
     gtk_css_provider_load_from_path(provider, "./pages/Everyone/style.css", NULL);
-    css_set(window, provider);
+
+    GtkWidget *label_name = GTK_WIDGET(gtk_builder_get_object(builder, "label_name"));
+    GtkWidget *list_box = GTK_WIDGET(gtk_builder_get_object(builder, "list_box"));
+
+    GtkListBox *list_mail = GTK_LIST_BOX(list_box);
+    for (int i = 0; i <= 4; i++)
+    {
+        GtkWidget *label = gtk_label_new(data_mail[i]);
+        GtkWidget *row = gtk_list_box_row_new();
+        GtkStyleContext *context = gtk_widget_get_style_context(row);
+        gtk_style_context_add_class(context, "container_item");
+
+        gtk_container_add(GTK_CONTAINER(row), label);
+        gtk_list_box_insert(list_mail, row, -1);
+    }
+    gtk_container_foreach(GTK_CONTAINER(list_box), (GtkCallback)css_set, provider);
+
     css_set(label_name, provider);
+    css_set(window, provider);
+
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    select_list = gtk_tree_view_get_selection(GTK_TREE_VIEW(list_everyone));
     gtk_widget_show_all(window);
-    gtk_window_maximize(GTK_WINDOW(window));
 
     gtk_main();
-    return 0;
+
+    return EXIT_SUCCESS;
 }
